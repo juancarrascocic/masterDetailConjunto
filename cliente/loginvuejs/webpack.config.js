f007fe63202432cbf61d30337eaafc007d2d3fc9
Andres <andrescic@outlook.com>
2017-08-22 15:55:04 +0200

    Añadido webpack y los componentes con single file component

---
+++ cliente/loginvuejs/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // other options...
+    entry:{
+      master: './components/master.vue',
+      root: './components/rootComponent.vue'
+    }, 
+output: {
+    filename: '[name]Component.js',
+
+  },
+
+  module: {
+    // module.rules is the same as module.loaders in 1.x
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          // `loaders` will overwrite the default loaders.
+          // The following config will cause all <script> tags without "lang"
+          // attribute to be loaded with coffee-loader
+          loaders: {
+            js: 'babel'
+          },
+        }
+      }
+    ]
+  }
+}
